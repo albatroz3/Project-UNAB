094e54fd2e0525a2cc23922f5dbc5db01eef8bde
AIDA <aida.barragan.mt@usa.edu.co>
2022-11-13 18:50:04 -0500

    mi primer commit

---
+++ src/main/java/project/reto345/crud/CostumeCrudRepository.java
@@ -0,0 +1,9 @@
+package project.reto345.crud;
+
+import org.springframework.data.repository.CrudRepository;
+import project.reto345.model.Costume;
+
+public interface CostumeCrudRepository extends CrudRepository<Costume, Integer> {
+
+}
+
